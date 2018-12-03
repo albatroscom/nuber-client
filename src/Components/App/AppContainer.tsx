16876d2d8e5c17abd15cba8953a78432c62b32df
seonggu.baek <seonggu.baek@nexstreaming.com>
2018-12-03 15:09:18 +0900

    Typescript and React Components

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { IS_LOGGED_IN } from './AppQueries';
+import AppPresenter from './AppPresenter';
 
-const AppContainer = ({ data }) => <div>{JSON.stringify(data)}</div>;
+const AppContainer = ({ data }) => <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
