d63385a1e0130e223676f54c0ada75bdd406a021
seonggu.baek <seonggu.baek@nexstreaming.com>
2018-12-04 15:22:15 +0900

    Typescript and Styled Components part Two ​

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import { IS_LOGGED_IN } from './AppQueries';
 import AppPresenter from './AppPresenter';
+import { ThemeProvider } from 'src/typed-components';
+import theme from 'src/theme';
 
-const AppContainer = ({ data }) => <AppPresenter isLoggedIn={data.auth.isLoggedIn} />;
+const AppContainer = ({ data }) => (
+<ThemeProvider theme={theme}>
+    <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+</ThemeProvider>
+);
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
--- src/typed-components.ts
+++ src/typed-components.ts
@@ -2,17 +2,16 @@ import * as styledComponents from "styled-components";
 import { ThemedStyledComponentsModule } from "styled-components";
 
 interface IThemeInterface {
-    primaryColor: string;
-    primaryColorInverted: string;
+  blueColor: string;
 }
 
 const {
   default: styled,
   css,
-  createGlobalStyle,
+  injectGlobal,
   keyframes,
   ThemeProvider
 } = styledComponents as ThemedStyledComponentsModule<IThemeInterface>;
 
-export { css, createGlobalStyle, keyframes, ThemeProvider };
+export { css, injectGlobal, keyframes, ThemeProvider };
 export default styled;
