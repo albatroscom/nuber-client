6001b90bdf859892d7ddb9f51dbbc987299f56bf
seonggu.baek <seonggu.baek@nexstreaming.com>
2018-12-11 18:39:25 +0900

    Magic with Apollo Codegen ( didn't fix issue )

--- src/Routes/PhoneLogin/PhoneQueries.tsx
+++
@@ -1,9 +0,0 @@
-import { gql } from 'apollo-boost';
-export const PHONE_SIGN_IN = gql`
-    mutaion startPhoneVerification(phoneNumber: String!) {
-        startPhoneVerification(phoneNumber: $phoneNumber) {
-            ok
-            error
-        }
-    }
-`;
