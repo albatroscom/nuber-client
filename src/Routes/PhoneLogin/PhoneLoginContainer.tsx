d908ea533dcd457b3b276227420910f471902b5e
seonggu.baek <seonggu.baek@nexstreaming.com>
2018-12-12 13:51:52 +0900

    PhoneLogin Mutation part Two

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-import { Mutation } from "react-apollo";
+import { Mutation, MutationUpdaterFn } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
 import { toast } from "react-toastify";
 import PhoneLoginPresenter from "./PhoneLoginPresenter";
 import { PHONE_SIGN_IN } from "./PhoneQueries.queries";
+import { startPhoneVerificationVariables, startPhoneVerification } from 'src/types/api';
 
 interface IState {
   countryCode: string;
   phoneNumber: string;
 }
 
-interface IMutationInterface {
-  phoneNumber: string;
-}
-
-class PhoneSignInMutation extends Mutation<any, IMutationInterface> {}
+class PhoneSignInMutation extends Mutation<startPhoneVerification, startPhoneVerificationVariables> {}
 
 class PhoneLoginContainer extends React.Component<
   RouteComponentProps<any>,
@@ -33,6 +30,7 @@ class PhoneLoginContainer extends React.Component<
         variables={{
           phoneNumber: `${countryCode}${phoneNumber}`
         }}
+        update={this.afterSubmit}
       >
         {(mutation, { loading }) => {
           const onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
@@ -70,6 +68,11 @@ class PhoneLoginContainer extends React.Component<
       [name]: value
     } as any);
   };
+
+  public afterSubmit: MutationUpdaterFn = (cache, data) => {
+    // tslint:disable-next-line
+    console.log(data);
+  }
 }
 
 export default PhoneLoginContainer;
